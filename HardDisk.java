6179e127e98a19468f327a0f010bc5c3271e37db
Lizonghan6 <72387587+Lizonghan6@users.noreply.github.com>
2020-10-08 20:08:36 +0800

    Add files via upload

---
+++ HardDisk.java
@@ -0,0 +1,11 @@
+package pc;
+
+public class HardDisk{
+    int amount;
+    double getAmount(){
+        return amount;
+    }
+    public void setAmount(int amount){
+        this.amount = amount;
+    }
+}
---
+++ Test.java
@@ -0,0 +1,13 @@
+package pc;
+public class Test{
+    public static void main(String args[]){
+        CPU cpu = new CPU();
+        HardDisk HD = new HardDisk();
+        cpu.setSpeed(2200);
+        HD.setAmount(200);
+        PC pc = new PC();
+        pc.setCPU(cpu);
+        pc.setHardDisk(HD);
+        pc.show();
+    }
+}
